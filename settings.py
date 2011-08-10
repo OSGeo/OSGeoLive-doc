2f76d8962d889695fe9854ba65aa2d22439f0a3f
Frank Gasdorf <fgdrf@users.sourceforge.net>
2011-08-10 20:24:53 +0000

    fixed python issue
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@6801 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 # module settings
 # downloadfilesize 
 # for osgeo-live-X.5.iso
-iso_size = 8.3
+iso_size = 4.3
 # for osgeo-live-mini-X.X.iso
 iso_mini_size = 3.2
 # for osgeo-vm-X.X.7z
-vm_sevenz_size = 2.9
+vm_7z_size = 2.9
+
+# required hard disk space
+req_hd_size = 10
