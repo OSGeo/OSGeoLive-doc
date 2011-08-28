02f894bab2c1e1f514133d8883ff25bcae0b116b
Alex Mandel <tech@wildintellect.com>
2011-08-28 05:01:54 +0000

    Forgot to update the file sizes for the download page, will make it to the web version
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@7133 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 # module settings
 # downloadfilesize 
 # for osgeo-live-X.5.iso
-iso_size = 4.3
+iso_size = 4.4
 # for osgeo-live-mini-X.X.iso
-iso_mini_size = 3.2
+iso_mini_size = 3.5
 # for osgeo-vm-X.X.7z
-vm_7z_size = 2.9
+vm_7z_size = 2.6
 
 # required hard disk space
 req_hd_size = 10
