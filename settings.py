6e7cd113db35278e29735a318a5a7a6ad13d939f
Frank Gasdorf <fgdrf@users.sourceforge.net>
2012-02-20 20:56:37 +0000

    update iso/vm/7 sizes in settings.py for docs
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@7647 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 # module settings
 # downloadfilesize 
-# for osgeo-live-X.5.iso
+# for osgeo-live-X.X.iso
 iso_size = 4.4
 # for osgeo-live-mini-X.X.iso
-iso_mini_size = 3.5
+iso_mini_size = 3.6
 # for osgeo-vm-X.X.7z
-vm_7z_size = 2.6
+vm_7z_size = 2.8
 
 # required hard disk space
 req_hd_size = 10
