3e431c16f0baeaee3fea91f499923e1498a087a2
Frank Gasdorf <fgdrf@users.sourceforge.net>
2014-01-08 19:43:26 +0000

    updated GeoDerver to 2.4.3
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@10980 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- conf.py
+++ conf.py
@@ -240,7 +240,7 @@ rst_epilog="""
 .. |osgeolive-appmenupath-geoserver| replace:: :menuselection:`Geospatial --> Web Services --> GeoServer --> Start GeoServer`
 .. |osgeolive-appmenupath-udig| replace:: :menuselection:`Geospatial --> Desktop GIS --> uDig`
 .. |version-deegree| replace:: 3.3.1
-.. |version-geoserver| replace:: 2.3.4
+.. |version-geoserver| replace:: 2.4.3
 .. |version-geokettle| replace:: 2.5
 .. |version-gvsig| replace:: 2.0
 .. |version-udig| replace:: 1.4.0b
