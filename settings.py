d793fd6de14ebd2439b28763f6b8327fca099751
Hamish Bowman <hamish_b yahoo com>
2014-03-10 20:29:21 +0000

    bump iso sizes
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@11363 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@
 # for osgeo-live-X.X.iso
 iso_size = 4.3
 # for osgeo-live-mini-X.X.iso
-iso_mini_size = 3.4
+iso_mini_size = 3.5
 # for osgeo-vm-X.X.7z
-vm_7z_size = 2.8
+vm_7z_size = 3.0
 
 # required hard disk space
-req_hd_size = 10
+req_hd_size = 20
