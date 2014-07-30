3251460fe3278aa538844914e45b26296b387a33
Benjamin Pross <b.pross@52north.org>
2014-07-30 07:18:24 +0000

    Updated variables for 52nWPS.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc@11625 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- conf.py
+++ conf.py
@@ -239,12 +239,13 @@ rst_epilog="""
 .. |osgeolive-vm-7z-size| replace:: %(vm_7z_size).1f GB
 .. |osgeolive-appmenupath-geoserver| replace:: :menuselection:`Geospatial --> Web Services --> GeoServer --> Start GeoServer`
 .. |osgeolive-appmenupath-udig| replace:: :menuselection:`Geospatial --> Desktop GIS --> uDig`
+.. |osgeolive-appmenupath-52nWPS| replace:: :menuselection:`Geospatial --> Web Services --> 52North --> Start 52North WPS`
 .. |version-deegree| replace:: 3.3.1
 .. |version-geoserver| replace:: 2.5
 .. |version-geokettle| replace:: 2.6
 .. |version-gvsig| replace:: 2.1
 .. |version-udig| replace:: 1.4.0b
-.. |version-52nWPS| replace:: 3.2.0
+.. |version-52nWPS| replace:: 3.3.0
 .. |version-52nSOS| replace:: 3.5.0
 .. |version-eoxserver| replace:: 0.3.2
 """ % {
