1502b0e2c23df928f016264b0a445db5c8ba37eb
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-08-26 21:04:58 +0300

    Updated image sizes (approximately)

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@
 # for osgeo-live-X.X.iso
 iso_size = 4.3
 # for osgeo-live-mini-X.X.iso
-iso_mini_size = 3.6
+iso_mini_size = 3.7
 # for osgeo-vm-X.X.7z
-vm_7z_size = 3.1
+vm_7z_size = 3.2
 
 # required hard disk space
 req_hd_size = 20
