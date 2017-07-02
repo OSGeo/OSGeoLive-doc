b237b777f033d46079da7117616d1e979f88d0c9
Cameron Shorter <cameron.shorter@gmail.com>
2017-07-03 07:07:36 +1000

    added more to the convert_osm_docs_to_quickstart.sh script

--- bin/convert_osm_docs_to_quickstarts.sh
+++ bin/convert_osm_docs_to_quickstarts.sh
@@ -26,14 +26,53 @@
 # web page "http://www.fsf.org/licenses/lgpl.html".
 ##################################################
 
-tmp=tmp
+echo aaa1
+# Get the source git repository:
+#git clone https://github.com/camerons/learnosm.git
+#git clone https://github.com/hotosm/learnosm.git
 
+echo aaa2
+# Move from OSM Directory to OSGeo-Live directory stucture
+mkdir -p OSGeoLive-doc
+mkdir -p OSGeoLive-doc/images
+mkdir -p OSGeoLive-doc/images/screenshots
+mkdir -p OSGeoLive-doc/images/screenshots/1024x768
+mkdir -p OSGeoLive-doc/en
+mkdir -p OSGeoLive-doc/en/quickstart
+
+git add OSGeoLive-doc
+got commit -m"Add OSGeoLive-doc directory"
+
+echo aaa3
+git mv images/beginner/id-editor* OSGeoLive-doc/images/screenshots/1024x768/
+git mv images/beginner/id-editor* OSGeoLive-doc/images/screenshots/1024x768/
+
+git mv _posts/en/1900-12-21-start-josm.md OSGeoLive-doc/en/quickstart/jsom_quickstart.rst
+git mv _posts/en/0200-12-23-id-editor.md OSGeoLive-doc/en/quickstart/ideditor_quickstart.rst
+echo aaa4
+
+git commit -m"Moved OSM id-editor and JSON guides to OSGeo-Live quickstart directory stucture" .
+
+echo aaa5
 # Convert from MD format to RST
+for file in \
+  OSGeoLive-doc/en/quickstart/jsom_quickstart.rst \
+  OSGeoLive-doc/en/quickstart/ideditor_quickstart.rst \
+; do
 
-#for file in 1900-12-21-start-josm 200-12-23-id-editor ; do
-for file in 1900-12-21-start-josm 200-12-23-id-editor ; do
-  pandoc --from=markdown --to=rst --output=${tmp} {$file}.md
+  echo aaa6
+  pandoc --from=markdown --to=rst --output=tmp {$file}
+  mv tmp ${file}
+  git commit -m"Changed from markdown to RST format" ${file}
 
+  echo aaa7
   # Change the location of images
-  sed -e's#/images/beginner/#/images/screenshots/1024x768/#g' ${tmp} > ${file}.rst
+  sed -e's#/images/beginner/#/images/screenshots/1024x768/#g' ${file} > tmp
+  mv tmp ${file}
+  git commit -m"changed image path to new location" ${file}
 done
+echo aaa8
+
+# Trim OSM's git history to just the directy to be copied to OSGeo-Live
+git filter-branch --subdirectory-filter OSGeoLive-doc -- --all
+
