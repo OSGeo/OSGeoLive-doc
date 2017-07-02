114a346e9769cf4f92f1b9db945f18b2af5b662a
camerons <cameron.shorter@gmail.com>
2017-07-02 15:51:57 +1000

    script/notes on pulling OSM docs into quickstarts

---
+++ bin/convert_osm_docs_to_quickstarts.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#################################################
+# 
+# Purpose: Partially convert source documentation from LearnOSM git
+# repository to OSGeo-Live git repository, keeping git history.
+# Source: http://gbayer.com/development/moving-files-from-one-git-repository-to-another-preserving-history/
+#
+# Then convert from MD format to RST format
+
+# Author:  Cameron Shorter
+#
+#################################################
+# Copyright (c) 2010-2016 Open Source Geospatial Foundation (OSGeo)
+# Copyright (c) 2009 Jirotech
+#
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+##################################################
+
+tmp=tmp
+
+# Convert from MD format to RST
+
+#for file in 1900-12-21-start-josm 200-12-23-id-editor ; do
+for file in 1900-12-21-start-josm 200-12-23-id-editor ; do
+  pandoc --from=markdown --to=rst --output=${tmp} {$file}.md
+
+  # Change the location of images
+  sed -e's#/images/beginner/#/images/screenshots/1024x768/#g' ${tmp} > ${file}.rst
+done
