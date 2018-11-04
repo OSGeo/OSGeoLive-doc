7e355d22f3e001cc9b7959268e727683d6a7ca0d
Cameron Shorter <cameron.shorter@gmail.com>
2018-11-04 13:01:16 +0300

    Resize presentation slide size, and corresponding image sizes. Images now fit in the Web Services slide of 14 icons table

--- doc/presentation/conf.py
+++ doc/presentation/conf.py
@@ -27,8 +27,8 @@ html_theme_options = {
 
     # The "normal" size of the presentation, aspect ratio will be preserved
     # when the presentation is scaled to fit different resolutions
-    'width': 960,
-    'height': 700,
+    'width': 1600,
+    'height': 1200,
 
     # Factor of the display size that should remain empty around the content
     'margin': 0.1,
