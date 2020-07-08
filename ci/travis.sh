8f8e08e9dd43490a109383426912ee1b5631d162
MarPetra <mvergaratrejo@gmail.com>
2020-07-08 16:42:54 -0500

    [travis] adding fi language for checking links

--- ci/travis.sh
+++ ci/travis.sh
@@ -6,9 +6,10 @@
 # ------------------------------------------------------------------------------
 if [ "$LINKCHECK" == "ON" ]
 then
-  cmake -DES=ON -DLINKCHECK=ON .. 
+  cmake -DES=ON -DFI=ON -DLINKCHECK=ON ..
   make linkcheck-en
   make linkcheck-es
+  make linkcheck-fi
 else
   cmake -DHTML=ON -DFI=ON -DIT=ON -DJA=ON -DES=ON -DFR=ON -DDE=ON -DHU=ON -@OSGeoLiveDoc_DEBUG=ON ..
   make
