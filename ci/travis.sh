2e1fb4052f273c931e93c89ddf0ac85902a63710
MarPetra <mvergaratrejo@gmail.com>
2020-08-26 13:47:01 -0500

    [de] checking links

--- ci/travis.sh
+++ ci/travis.sh
@@ -6,10 +6,12 @@
 # ------------------------------------------------------------------------------
 if [ "$LINKCHECK" == "ON" ]
 then
-  cmake -DES=ON -DFI=ON -DLINKCHECK=ON ..
+  cmake -DES=ON -DFI=ON -DDE=ON -DLINKCHECK=ON ..
   make linkcheck-en
   make linkcheck-es
   make linkcheck-fi
+  make linkcheck-de
+
 else
   cmake -DHTML=ON -DFI=ON -DIT=ON -DJA=ON -DES=ON -DFR=ON -DDE=ON -DHU=ON -@OSGeoLiveDoc_DEBUG=ON ..
   make
