4a66ce709947f359ee989c473b1719b23b2a6b91
cvvergara <cvvergara@users.noreply.github.com>
2020-11-23 17:25:21 +0000

    deploy: 3d34dce549b351bde780fe35fd05b21bcc79e8af

--- _static/cmake_install.cmake
+++ _static/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/travis/build/OSGeo/OSGeoLive-doc/doc/_themes/overview/static
+# Install script for directory: /home/runner/work/OSGeoLive-doc/OSGeoLive-doc/doc/_themes/overview/static
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,9 +32,14 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/travis/build/OSGeo/OSGeoLive-doc/build/doc/_themes/overview/static/img/cmake_install.cmake")
+  include("/home/runner/work/OSGeoLive-doc/OSGeoLive-doc/build/doc/_themes/overview/static/img/cmake_install.cmake")
 
 endif()
 
