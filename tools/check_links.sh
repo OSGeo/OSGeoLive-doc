1f7b06c71427a5f9d4d0c9d9034118fe24f6bb96
cvvergara <vicky@georepublic.de>
2020-12-15 17:19:59 -0600

    adding some scripts for translation check

---
+++ tools/check_links.sh
@@ -0,0 +1,10 @@
+#TODO add a message when the language is missing
+
+mkdir -p build
+cd build || exit 1
+
+
+cmake -DHTML=ON -DFI=ON -DFR=ON -DDE=ON -DHU=ON -DES=ON -DJA=ON -DIT=ON -DLINKCHECK=ON ..
+make linkcheck-$1
+
+
