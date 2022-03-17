73e6c9c8660124020e4c3f89ebbdefd98f4d37eb
cvvergara <vicky@georepublic.de>
2022-03-17 13:44:49 -0600

    using a script

--- ci/build-pages.sh
+++ ci/build-pages.sh
@@ -5,9 +5,13 @@
 # Main configuration
 # ------------------------------------------------------------------------------
 
+DIR=$(git rev-parse --show-toplevel)
+pushd "${DIR}" > /dev/null || exit
+pushd build > /dev/null || exit
 cmake -DHTML=ON -DFI=ON -DIT=ON -DJA=ON -DES=ON -DFR=ON -DDE=ON -DHU=ON -DOSGeoLiveDoc_DEBUG=ON ..
 make
-cd ..
+popd 
 bash scripts/clean-images.sh
 touch build/doc/_build/html/.nojekyll # needed for gh pages to keep dirs starting with _
 du -h build/doc/_build/html
+popd
