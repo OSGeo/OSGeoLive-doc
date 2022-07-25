c305b4a4606cd3a7302fa9a9b514c152448cf3ff
cvvergara <cvvergara@users.noreply.github.com>
2022-07-25 19:20:57 +0000

    deploy: c6efd19eb8eb7a0e5263c2d5a921baf2a812aa12

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,14 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'August, 2022',
+    VERSION: '15.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'revealjs',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
